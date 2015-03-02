69fe924773d2ee24e5f78f08b01418d242991330
Andrew Robbins <and_j_rob@yahoo.com>
2015-03-01 22:51:30 -0500

    Documentation

--- implementors/rustc-serialize/hex/trait.ToHex.js
+++ implementors/rustc-serialize/hex/trait.ToHex.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors['rustc-serialize'] = [];
+implementors['cryptoil'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='rustc-serialize/hex/trait.ToHex.html' title='rustc-serialize::hex::ToHex'>ToHex</a> for <a class='struct' href='cryptoil/sha/sha256/struct.Sha256.html' title='cryptoil::sha::sha256::Sha256'>Sha256</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
